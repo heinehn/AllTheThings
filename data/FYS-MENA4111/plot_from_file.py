96ce480fc5b99d6f95f446e9bbeac9726c6d6884
heinehn <heinehn@student.matnat.uio.no>
2016-11-17 16:59:33 +0100

    plots for potensial

--- data/FYS-MENA4111/plot_from_file.py
+++ data/FYS-MENA4111/plot_from_file.py
@@ -9,15 +9,17 @@ file2 = "vaspout_GaN_cutoff.txt"
 file3 = "GaN_slabConvergeVac.txt"
 file4 = "GaN_vac_converge.txt"
 file5 = "10AGaNslabCutoff.txt"
-file6 = "LOCPOT_line10A.txt"
+file_pot_10 = "LOCPOT_line10A.txt"
+file_pot_12 = "LOCPOT_line12A.txt"
+file_pot_14 = "LOCPOT_line14A.txt"
 data = np.loadtxt(file, dtype = 'float', skiprows = 1, usecols = (1,2))
 #vaspout = np.loadtxt(file, dtype = 'float', skiprows = 1, usecols = (0,1,2,3))
 bulk_cut = np.loadtxt(file, dtype = 'float', skiprows = 1, usecols = (0,1,2,3))
 slab_cut = np.loadtxt(file5, dtype = 'float', skiprows = 1, usecols = (0,1,2,3))
-slab_locpot = np.loadtxt(file6, dtype = 'float', skiprows = 4)
-print slab_locpot[:,1]
+slab_locpot10 = np.loadtxt(file_pot_10, dtype = 'float', skiprows = 4)
+slab_locpot12 = np.loadtxt(file_pot_12, dtype = 'float', skiprows = 4)
+slab_locpot14 = np.loadtxt(file_pot_14, dtype = 'float', skiprows = 4)
 
-plt.plot(slab_locpot[:,0],slab_locpot[:,1])
 
 
 """
@@ -37,7 +39,6 @@ diff_bulk_slab = bulk_cut/3 - slab_cut/6
 print diff_bulk_slab[:,3]
 
 
-
 plt.figure(1)
 plt.plot(en_diff, label = '$\Delta$E')
 #plt.plot(press_diff, label = '$\Delta$P')
@@ -50,8 +51,50 @@ plt.ylabel(r'$\Delta$',size = 20, labelpad = 5)
 plt.title( r'Convergence for different vacuum distance in $GaN$',size = 25 )
 plt.legend(loc='smart',fontsize = 15)
 #plt.savefig('plot_GaN_vacuum_relax.png',bbox_inches='tight')
+"""
+
+plt.figure(1)
+plt.plot(slab_locpot10[:,0],slab_locpot10[:,1])
+#plt.plot(press_diff, label = '$\Delta$P')
+#plt.plot(mxF_diff, label = '$\Delta$mxF')
+plt.grid('on')
+plt.xticks( fontsize = 20)
+plt.yticks( fontsize = 20)
+plt.xlabel(r'Distance [A]', size = 20,labelpad= 5 )
+plt.ylabel(r'Potensial [V]',size = 20, labelpad = 5)
+plt.title( r'Potensial vs distance in $GaN$ slab',size = 25 )
+plt.legend(loc='smart',fontsize = 15)
+plt.savefig('plot_GaN_potensial_vacuum10.eps',bbox_inches='tight')
 
 
+plt.figure(2)
+plt.plot(slab_locpot12[:,0],slab_locpot12[:,1])
+#plt.plot(press_diff, label = '$\Delta$P')
+#plt.plot(mxF_diff, label = '$\Delta$mxF')
+plt.grid('on')
+plt.xticks( fontsize = 20)
+plt.yticks( fontsize = 20)
+plt.xlabel(r'Distance [A]', size = 20,labelpad= 5 )
+plt.ylabel(r'Potensial [V]',size = 20, labelpad = 5)
+plt.title( r'Potensial vs distance in $GaN$ slab',size = 25 )
+plt.legend(loc='smart',fontsize = 15)
+plt.savefig('plot_GaN_potensial_vacuum12.eps',bbox_inches='tight')
+
+plt.figure(3)
+plt.plot(slab_locpot14[:,0],slab_locpot14[:,1])
+#plt.plot(press_diff, label = '$\Delta$P')
+#plt.plot(mxF_diff, label = '$\Delta$mxF')
+plt.grid('on')
+plt.xticks( fontsize = 20)
+plt.yticks( fontsize = 20)
+plt.xlabel(r'Distance [A]', size = 20,labelpad= 5 )
+plt.ylabel(r'Potensial [V]',size = 20, labelpad = 5)
+plt.title( r'Potensial vs distance in $GaN$ slab',size = 25 )
+plt.legend(loc='smart',fontsize = 15)
+plt.savefig('plot_GaN_potensial_vacuum14.eps',bbox_inches='tight')
+
+
+"""
 #energy = data[:,1]
 #en_diff = absdiff(energy)
 cutoff_range = range(200,850,50)
